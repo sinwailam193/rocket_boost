7f1955060928df64d6062bba5fc74e4ac52d8bf8
sinwailam193 <8454621+sinwailam193@users.noreply.github.com>
2025-05-18 21:31:13 +0800

    Update with rotation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,8 +3,10 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float thrustStrength = 20f;
+    [SerializeField] float thrustStrength = 1000f;
+    [SerializeField] float rotationSpeed = 100f;
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
     Rigidbody rb;
 
     private void Start()
@@ -15,13 +17,32 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     private void FixedUpdate()
     {
+        HandleThrust();
+        HandleRotation();
+    }
+
+    private void HandleThrust()
+    {
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+
+    private void HandleRotation()
+    {
+        float rotationValue = rotation.ReadValue<float>();
+
+        if (rotationValue != 0)
+        {
+            Vector3 rotationDirection = rotationValue > 0 ? Vector3.forward : Vector3.back;
+
+            transform.Rotate(rotationDirection * rotationSpeed * Time.fixedDeltaTime);
         }
     }
 }
